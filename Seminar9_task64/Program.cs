b6ba356193445e23254a5c547baa4c3bd1ecfc56
Saygat <aleksandr.filimonov@sequenia.com>
2022-09-28 15:38:33 +0700

    Add homework from Seminar 9

---
+++ Seminar9_task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// Печать строки.
+void PrintData(string data, string pref = "", string post = "", string end = "\n")
+{
+    Console.Write($"{pref}{data}{post}{end}");
+}
+
+// Метод считывания данных пользователя
+uint ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Возвращаем значение
+    return uint.Parse(Console.ReadLine() ?? "");
+}
+
+// Реверсивная последовательность чисел.
+string SequenceReversed(uint num)
+{
+    if (num == 0) return string.Empty;
+    return $"{num} {SequenceReversed(num - 1)}";
+}
+
+PrintData(SequenceReversed(ReadData("Введите число: ")));
---
+++ Seminar9_task66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Печать строки.
+void PrintData(string data, string pref = "", string post = "", string end = "\n")
+{
+    Console.Write($"{pref}{data}{post}{end}");
+}
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Возвращаем значение
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+// Сумма чисел в диапозоне [,]
+int SumRec(int start, int stop)
+{
+    if (start > stop)
+        return 0;
+    return SumRec(start + 1, stop) + start;
+}
+int start = ReadData("Введите первое число: ");
+int stop = ReadData("Введите второе число: ");
+PrintData(SumRec(start, stop).ToString(), pref: $"Сумма чисел от {start} до {stop} равна: ");
